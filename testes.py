bb9a73fc2a0b9baaa622d8e72e817079101b7b08
Vinícius Machado <viniciusfm1@outlook.com>
2019-01-22 22:08:25 -0200

    add test ticker

--- testes.py
+++ testes.py
@@ -5,4 +5,9 @@ exchange = Braziliex('ltc_brl')
 
 ticker = exchange.ticker()
 
-print(ticker)
+print('# TICKER #------------------------------')
+if ticker['active'] == 1:
+    print('última negociação :',ticker['last'])
+    print('volume 24h        :',ticker['baseVolume24'])
+    print('compra            :',ticker['highestBid'])
+    print('venda             :',ticker['lowestAsk'])
