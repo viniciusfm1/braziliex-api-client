94a8aba2429a073a0442a6021f784110e996c4ff
Vinícius Machado <viniciusfm1@outlook.com>
2019-01-27 19:11:18 -0200

    initial refactoring

--- braziliex.py
+++ braziliex.py
@@ -21,15 +21,29 @@ class Braziliex:
         self.secret = secret
         self.privateUrl  = 'https://braziliex.com/api/v1/private'
     
-    def depositAddress(self):
-        
-        """Used to get a deposit address by market."""
-
-        data = urllib.parse.urlencode({'command': 'deposit_address', 'currency': str(self.par.split('_',1)[0]), 'nonce': int(time() * 1000)}).encode('utf-8')
+    def post(self, data):
+        data['nonce'] = int(time() * 1000)
+        data = urllib.parse.urlencode(data).encode('utf-8')
         sign = hmac.new(str.encode(self.secret, 'utf-8'), data, digestmod = hashlib.sha512).hexdigest()
         headers = {'Content-type': 'application/x-www-form-urlencoded', 'Sign': sign, 'Key': self.key}
         response = requests.post(self.privateUrl, data = data, headers = headers)
         return response.json()
+
+    def get(self):
+        pass
+
+    def balance(self):      
+        """Returns all of your balances, including available balance, balance on orders, 
+        and the estimated BTC value of your balance."""
+        
+        data = {'command': 'balance'}
+        return self.post(data)
+
+    def depositAddress(self):
+        """Used to get a deposit address by market."""
+        
+        data = {'command': 'deposit_address'}
+        return self.post(data)
 
     def ticker(self, method = 'ticker'):
         
@@ -48,17 +62,6 @@ class Braziliex:
     def trades(self, method = 'tradehistory'):
         """Used to get retrieve the last trades."""
         response = requests.get(self.url.format(method = method, param = self.par))
-        return response.json()
-
-    def balance(self, command='balance'):      
-
-        """Returns all of your balances, including available balance, balance on orders, 
-        and the estimated BTC value of your balance."""
-
-        data = urllib.parse.urlencode({'command': command, 'nonce': int(time() * 1000)}).encode('utf-8')
-        sign = hmac.new(str.encode(self.secret, 'utf-8'), data, digestmod = hashlib.sha512).hexdigest()
-        headers = {'Content-type': 'application/x-www-form-urlencoded', 'Sign': sign, 'Key': self.key}
-        response = requests.post(self.privateUrl, data = data, headers = headers)
         return response.json()
 
     def createOrder(self, command, amount, price):
